19591b40d8ac7f5f8d93634c1ad42a1ff8204b57
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-31 20:49:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "W";
 const adminprefix = "#";
-const developers = ["461059496861630465"]//Toxic Codes
+const developers = ["454421822478352416"]//Toxic Codes
 client.on('message', message => {//Toxic Codes
     var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
       if (!developers.includes(message.author.id)) return;
